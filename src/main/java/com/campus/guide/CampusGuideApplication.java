f812f2dc012859b6db4356d0c450cc6544c26366
김은찬 <ksadh10@naver.com>
2026-03-19 16:42:53 +0900

    First commit: 앙기모

---
+++ src/main/java/com/campus/guide/CampusGuideApplication.java
@@ -0,0 +1,13 @@
+package com.campus.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campus/guide/CampusGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
